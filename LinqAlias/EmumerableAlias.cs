8edbdb1755f2c0f9e6928700458bc95506b742fd
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2019-02-14 00:41:56 +0900

    added more functions
    * Reduce - Aggregate
    * Fold - Aggregate
    * ForAll - All
    * Exists - Any

--- LinqAlias/EmumerableAlias.cs
+++ LinqAlias/EmumerableAlias.cs
@@ -18,5 +18,26 @@ namespace LinqAlias
             this IEnumerable<TSource> source,
             Func<TSource, IEnumerable<TResult>> selector
         ) => source.SelectMany(selector);
+
+        public static bool ForAll<TSource>(
+            this IEnumerable<TSource> source,
+            Func<TSource, bool> predicate
+        ) => source.All(predicate);
+
+        public static bool Exists<TSource>(
+            this IEnumerable<TSource> source,
+            Func<TSource, bool> selector
+        ) => source.Any(selector);
+
+        public static TSource Reduce<TSource>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TSource, TSource> func
+        ) => source.Aggregate(func);
+
+        public static TAccumulate Fold<TSource, TAccumulate>(
+            this IEnumerable<TSource> source,
+            TAccumulate seed,
+            Func<TAccumulate, TSource, TAccumulate> func
+        ) => source.Aggregate(seed, func);
     }
 }
